03d062da6c355ad328dad1bdcde768b047b1a717
xzp <xingzepeng@geo-compass.com>
2022-04-22 21:54:06 +0800

    feat: 基本生命周期

--- src/index.js
+++ src/index.js
@@ -1,56 +1,56 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-unreachable */
-import React from './react';
-import ReactDOM from './react-dom';
+// import React from "react";
+// import ReactDOM from "react-dom";
+import React from "./react";
+import ReactDOM from "./react-dom";
 class Counter extends React.Component {
-  render() { return <div>Counter</div> }
-}
-function Func(props, forwardRef) {
-  return <div ref={forwardRef}>Func</div>
-}
-const ForwardFunc = React.forwardRef(Func);
-console.log(ForwardFunc);
-let lastCounter;
-class Sum extends React.Component {
+  static defaultProps = {
+    name: "珠峰架构",
+  };
   constructor(props) {
-    super(props)
-    this.a = React.createRef();//{current:null}
-    this.b = { current: null };
-    this.counter = React.createRef();
-    this.result = React.createRef();
-    this.any = React.createRef();
-    this.func = React.createRef();
+    super(props);
     this.state = { number: 0 };
+    console.log("Counter 1.constructor");
+  }
+  UNSAFE_componentWillMount() {
+    console.log("Counter 2.componentWillMount");
+  }
+  componentDidMount() {
+    console.log("Counter 4.componentDidMount");
   }
   handleClick = () => {
-    let valueA = this.a.current.value;
-    let valueB = this.b.current.value;
-    this.result.current.value = valueA + valueB;
+    this.setState({ number: this.state.number + 1 });
+  };
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("Counter 5.shouldComponentUpdate");
+    return nextState.number % 2 === 0;
   }
-  onClick = () => {
-    console.log('this.func.current', this.func.current);
-    lastCounter = this.counter.current
-    this.setState(state => ({ number: state.number + 1 }),  () => {
-      console.log(this.counter.current === lastCounter);
-    });
+  UNSAFE_componentWillUpdate() {
+    console.log("Counter 6.componentWillUpdate");
+  }
+  componentDidUpdate(newProps, newState) {
+    console.log("Counter 7.componentDidUpdate");
   }
   render() {
-    
+    console.log("Counter 3.render");
     return (
       <div>
-        <Counter ref={this.counter} />
-        <ForwardFunc ref={this.func} />
-        <button onClick={this.onClick}>{this.state.number}</button>
-        <input ref={this.a} />+<input ref={this.b} /><button onClick={this.handleClick}>=</button><input ref={this.result} />
+        <p>{this.state.number}</p>
+        <button onClick={this.handleClick}>+</button>
       </div>
-    )
+    );
   }
-
 }
-ReactDOM.render(<Sum />, document.getElementById('root'));
+ReactDOM.render(<Counter />, document.getElementById("root"));
 
 /**
- * Warning: Function components cannot be given refs. 
- * Attempts to access this ref will fail.
- *  Did you mean to use React.forwardRef()?
+组件的生命周期
+Counter 1.constructor
+Counter 2.componentWillMount
+Counter 3.render
+Counter 4.componentDidMount
+Counter 5.shouldComponentUpdate
+Counter 5.shouldComponentUpdate
+Counter 6.componentWillUpdate
+Counter 3.render
+Counter 7.componentDidUpdate
  */
--- src/react-dom.js
+++ src/react-dom.js
@@ -1,8 +1,8 @@
-import { REACT_TEXT, ReactComponent, REACT_FORWARD_REF } from "./constants"
-import {addEvent} from './event'
+import { REACT_TEXT, ReactComponent, REACT_FORWARD_REF } from "./constants";
+import { addEvent } from "./event";
 
 function render(vdom, container) {
-  mount(vdom, container)
+  mount(vdom, container);
 }
 
 /**
@@ -11,143 +11,153 @@ function render(vdom, container) {
  * @param {*} container 容器
  */
 function mount(vdom, container) {
-  let newDOM = createDOM(vdom)
-  container.appendChild(newDOM)
+  let newDOM = createDOM(vdom);
+  container.appendChild(newDOM);
+  if (newDOM.componentDidMount) {
+    newDOM.componentDidMount();
+  }
 }
 /**
  * 根据虚拟DOM转成真实DOM
- * @param {*} vdom 
+ * @param {*} vdom
  */
 function createDOM(vdom) {
-  let {type, props, ref} = vdom
-  let dom //真实DOM
+  let { type, props, ref } = vdom;
+  let dom; //真实DOM
 
   if (type && type.$$typeof === REACT_FORWARD_REF) {
-    return mountForwardComponent(vdom)
+    return mountForwardComponent(vdom);
   }
 
   if (type === REACT_TEXT) {
-    dom = document.createTextNode(props)
-  }else if(typeof type === 'function'){
+    dom = document.createTextNode(props);
+  } else if (typeof type === "function") {
     if (type.isReactComponent === ReactComponent) {
-      return mountClassComponent(vdom)
-    }else{
-      return mountFunctionComponent(vdom)
+      return mountClassComponent(vdom);
+    } else {
+      return mountFunctionComponent(vdom);
     }
-  }else{
-    dom = document.createElement(type)
+  } else {
+    dom = document.createElement(type);
   }
   if (props) {
-    updateProps(dom, {}, props)
-    const children = props.children
-    if (typeof children === 'object' && children.type) {
-      mount(children, dom)
-    }else if(Array.isArray(children)){
-      reconcileChildren(children, dom)
+    updateProps(dom, {}, props);
+    const children = props.children;
+    if (typeof children === "object" && children.type) {
+      mount(children, dom);
+    } else if (Array.isArray(children)) {
+      reconcileChildren(children, dom);
     }
   }
-  vdom.dom = dom //让vdom的dom属性指向真实的DOM
+  vdom.dom = dom; //让vdom的dom属性指向真实的DOM
 
-  if(ref) ref.current = dom
-  return dom
+  if (ref) ref.current = dom;
+  return dom;
 }
 function mountForwardComponent(vdom) {
-  let {type, props, ref} = vdom
-  let renderVdom = type.render(props, ref)
-  vdom.oldRenderVdom = renderVdom
-  return createDOM(renderVdom)
+  let { type, props, ref } = vdom;
+  let renderVdom = type.render(props, ref);
+  vdom.oldRenderVdom = renderVdom;
+  return createDOM(renderVdom);
 }
 
 function mountFunctionComponent(vdom) {
-  let {type, props} = vdom
+  let { type, props } = vdom;
 
-  let renderVdom = type(props)
+  let renderVdom = type(props);
   //记住老的虚拟dom 方便后面domdiff
-  vdom.oldRenderVdom = renderVdom
-  return createDOM(renderVdom)
+  vdom.oldRenderVdom = renderVdom;
+  return createDOM(renderVdom);
 }
 
 function mountClassComponent(vdom) {
-  let {type:ClassComponent, props, ref} = vdom
-  
-  let classInstance = new ClassComponent(props)
-  if(ref) ref.current = classInstance
-  let renderVdom = classInstance.render()
-  vdom.classInstance = classInstance
+  let { type: ClassComponent, props, ref } = vdom;
+
+  let classInstance = new ClassComponent(props);
+
+  //组件将要挂载
+  if (classInstance.UNSAFE_componentWillMount) {
+    classInstance.UNSAFE_componentWillMount();
+  }
+  if (ref) ref.current = classInstance;
+  let renderVdom = classInstance.render();
+  vdom.classInstance = classInstance;
   //把上一次render渲染的vdom保留
-  vdom.oldRenderVdom = classInstance.oldRenderVdom =renderVdom
-  return createDOM(renderVdom)
+  vdom.oldRenderVdom = classInstance.oldRenderVdom = renderVdom;
+
+  let dom = createDOM(renderVdom);
+
+  if (classInstance.componentDidMount) {
+    dom.componentDidMount = classInstance.componentDidMount.bind(classInstance);
+  }
+
+  return dom;
 }
 
 function reconcileChildren(children, parentDOM) {
   children.forEach((child) => {
-    mount(child, parentDOM)
-  })
+    mount(child, parentDOM);
+  });
 }
 
 /**
  * 跟新真实DOM的属性
- * @param {*} dom 
- * @param {*} oldProps 
- * @param {*} newProps 
+ * @param {*} dom
+ * @param {*} oldProps
+ * @param {*} newProps
  */
 function updateProps(dom, oldProps = {}, newProps = {}) {
-  for(let key in newProps){
+  for (let key in newProps) {
     //属性中的children属性不在此处理
-    if (key === 'children') {
-      continue
-    }else if (key === 'style') {
-      let styleObj = newProps[key]
-      for(let attr in styleObj){
-        dom.style[attr] = styleObj[attr]
+    if (key === "children") {
+      continue;
+    } else if (key === "style") {
+      let styleObj = newProps[key];
+      for (let attr in styleObj) {
+        dom.style[attr] = styleObj[attr];
       }
-    }else if(/^on[A-Z].*/.test(key)){
+    } else if (/^on[A-Z].*/.test(key)) {
       // dom[key.toLowerCase()] = newProps[key]
-      addEvent(dom, key.toLowerCase(), newProps[key])
-    }else{
-       dom[key] = newProps
+      addEvent(dom, key.toLowerCase(), newProps[key]);
+    } else {
+      dom[key] = newProps;
     }
   }
   //如果属性在老的属性里，新的属性没有，需要从真实DOM中删除
-  for(let key in oldProps){
-    if(!newProps.hasOwnProperty(key)){
-      dom[key] = null
+  for (let key in oldProps) {
+    if (!newProps.hasOwnProperty(key)) {
+      dom[key] = null;
     }
   }
 }
 
-
-
 /**
  * dom diff 对比
- * @param {*} parentDom 
- * @param {*} oldVdom 
- * @param {*} newVdom 
+ * @param {*} parentDom
+ * @param {*} oldVdom
+ * @param {*} newVdom
  */
 export function compareTwoVdom(parentDom, oldVdom, newVdom) {
-  
   //获取老的真实DOM
-  let oldDOM = findDom(oldVdom)
-  let newDom = createDOM(newVdom)
-  parentDom.replaceChild(newDom, oldDOM)
+  let oldDOM = findDom(oldVdom);
+  let newDom = createDOM(newVdom);
+  parentDom.replaceChild(newDom, oldDOM);
 }
- 
+
 export function findDom(vdom) {
-  if(!vdom) return vdom
+  if (!vdom) return vdom;
 
   //如果有dom说明是原生标签 span div
   if (vdom.dom) {
-    return vdom.dom
-  }else{
-    let oldRenderVdom = vdom.oldRenderVdom
-    return findDom(oldRenderVdom)
+    return vdom.dom;
+  } else {
+    let oldRenderVdom = vdom.oldRenderVdom;
+    return findDom(oldRenderVdom);
   }
-  
-  
 }
 
 const ReactDOM = {
-  render
-}
+  render,
+};
 
-export default ReactDOM
+export default ReactDOM;
